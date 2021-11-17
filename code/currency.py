febff2a2b1c52b37031d6dd307e91f3a77902038
Nikita Flesakov <45170829+Ap3o@users.noreply.github.com>
2021-11-18 01:06:35 +0500

    Добавлены type hinting'и

--- code/currency.py
+++ code/currency.py
@@ -6,7 +6,7 @@ import os
 import requests
 
 
-def get_request_json(conv):
+def get_request_json(conv: str) -> requests.Request:
     try:
         request = requests.get(config.CURRCONV_LINK,
                                params={"q": conv, "compact": "ultra",
@@ -26,7 +26,7 @@ class Currencies:
         """
         self.cached_in_redis = []
 
-    def set_new_currency(self, conv):
+    def set_new_currency(self, conv: str) -> None:
         request = get_request_json(conv)
         if conv not in request:
             # Вызываем ошибку для того чтобы исключить дальнейшую проверку в апи
@@ -35,13 +35,13 @@ class Currencies:
             redis_connector.redis_instance.set(conv, request[conv])
             self.cached_in_redis.append(conv)
 
-    def update_currency_data(self):
+    def update_currency_data(self) -> None:
         # Из-за ограничения бесплатной версии отправляю запросы по одному.
         for value in self.cached_in_redis:
             request = get_request_json(value)
             redis_connector.redis_instance.set(value, request[value])
 
-    def flush_db(self):
+    def flush_db(self) -> None:
         redis_connector.redis_instance.flushdb(asynchronous=True)
         self.cached_in_redis = []
 

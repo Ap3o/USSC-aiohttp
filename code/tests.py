21b7bcab2fff82995dae4d695c7df64cc941dc85
Nikita Flesakov <45170829+Ap3o@users.noreply.github.com>
2021-11-18 01:55:35 +0500

    Тесты

---
+++ code/tests.py
@@ -0,0 +1,57 @@
+from aiohttp.test_utils import AioHTTPTestCase
+import json
+
+import application
+
+
+class USSCTestCase(AioHTTPTestCase):
+    async def get_application(self):
+        return application.create_app()
+
+    async def test_get_1(self):
+        async with self.client.request("GET", "/convert/?from=RRR&to=USD&amount=42") as response:
+            self.assertEqual(response.status, 200)
+            text = await response.text()
+            self.assertTrue("error" in json.loads(text))
+
+    async def test_get_2(self):
+        async with self.client.request("GET", "/convert/?from=RUB&to=USD&amount=42") as response:
+            self.assertEqual(response.status, 200)
+            text = await response.text()
+            self.assertTrue("result" in json.loads(text))
+
+    async def test_get_3(self):
+        async with self.client.request("GET", "/convert/?from=RUB&to=RRR&amount=42") as response:
+            self.assertEqual(response.status, 200)
+            text = await response.text()
+            self.assertTrue("error" in json.loads(text))
+
+    async def test_get_4(self):
+        async with self.client.request("GET", "/convert/?from=RUB&to=USD&amount=fff") as response:
+            self.assertEqual(response.status, 200)
+            text = await response.text()
+            self.assertTrue("error" in json.loads(text))
+
+    async def test_flush(self):
+        async with self.client.request("POST", "/database/", params={"merge": 0}) as response:
+            self.assertEqual(response.status, 200)
+            text = await response.text()
+            self.assertTrue("result" in json.loads(text))
+
+    async def test_post_1(self):
+        async with self.client.request("POST", "/database/", params={"merge": 1}) as response:
+            self.assertEqual(response.status, 200)
+            text = await response.text()
+            self.assertTrue("result" in json.loads(text))
+
+    async def test_post_2(self):
+        async with self.client.request("POST", "/database/", params={"merge": "ff"}) as response:
+            self.assertEqual(response.status, 200)
+            text = await response.text()
+            self.assertTrue("error" in json.loads(text))
+
+    async def test_post_3(self):
+        async with self.client.request("POST", "/database/") as response:
+            self.assertEqual(response.status, 200)
+            text = await response.text()
+            self.assertTrue("error" in json.loads(text))
